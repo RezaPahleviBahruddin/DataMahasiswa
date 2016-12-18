1ffdca4220cd1f664bf2e1336fef49cea082e3fd
RezaPahlevi <chester_phalevi@windowslive.com>
2016-12-18 09:19:29 +0700

    Fixed login

--- dbconfig.php
+++ dbconfig.php
@@ -14,12 +14,13 @@
 			{
 				$this->con = null;
 				try {
-					$this->con = new PDO("mysql:host=".$this->host.";dbname=".$db_name, $this->username, $this->password);
+					$this->con = new PDO("mysql:host=".$this->host.";dbname=".$this->db_name, $this->username, $this->password);
 					$this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				} catch (PDOException $e) {
 					echo "Connection error: ".$e->getMessage();
 				}
+
+				return $this->con;
 			}
-			return $this->con;
 		}	
 ?>
